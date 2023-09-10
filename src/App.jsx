acaf7b87d340b6baad44299ae53641b344e81b90
slowcheet4h <47327665+slowcheet4h@users.noreply.github.com>
2023-09-10 21:55:20 +0300

    some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Main from './pages/Main'
 import Playerlist from './pages/Playerlist'
+import Maintenance from './pages/Maintenance';
 
 function App() {
+  /* change if server is in maintenance */
+  let maintenance = false;
 
   return (
-    <>
+    maintenance ? (<Maintenance></Maintenance>) : (<>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/playerlist" element={<Playerlist />} />
         <Route index element={<Main />} />
       </Routes>
-    </>
+    </>)
   )
 }
 
