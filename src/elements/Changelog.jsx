f36a2a78a83c80f996bfb3315562e0195e9517ba
slowcheet4h <47327665+slowcheet4h@users.noreply.github.com>
2023-09-10 21:52:23 +0300

    initial commit

--- src/elements/Changelog.jsx
+++ src/elements/Changelog.jsx
@@ -3,12 +3,8 @@ export default function Changelog() {
     return (<>
         <div className="changelog">
             <ul>
-                <h6>04.09.2023</h6>
-                <li className="added"><Check /><span>added cats</span></li>
-                <li className="fixed"><Edit3 /><span>fixed dura-patc</span>h</li>
-                <li className="removed"><Trash2 /><span>removed firion area</span></li>
-                <li className="removed"><Trash2 /><span>removed semoteo</span></li>
-                <li className="added"><Check /><span>added cats</span></li>
+                <h6>10.09.2023</h6>
+                <li className="added"><Check /><span>website is online!</span></li>
                 <h6>12.02.2023</h6>
                 <li className="fixed"><Edit3 /><span>fixed dura-patc</span>h</li>
                 <li className="removed"><Trash2 /><span>removed firion area</span></li>
